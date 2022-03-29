88c249334ca2fc368eb5a9a42bec07da58642d15
Jan-Niklas Meier <jan@jansblog.org>
2022-03-29 16:53:23 +0200

    Fixing version command

--- cmd/version.go
+++ cmd/version.go
@@ -2,9 +2,15 @@ package cmd
 
 import (
 	"fmt"
-	"runtime/debug"
 
 	"github.com/spf13/cobra"
+)
+
+var (
+	Version = ""
+	Commit  = ""
+	Date    = ""
+	BuiltBy = ""
 )
 
 func init() {
@@ -15,8 +21,6 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print version information",
 	Run: func(cmd *cobra.Command, args []string) {
-		if info, ok := debug.ReadBuildInfo(); ok {
-			fmt.Printf("version: %s\n", info.Main.Version)
-		}
+		fmt.Fprintf(cmd.OutOrStdout(), "props %s, commit %s, built at %s by %s\n", Version, Commit, Date, BuiltBy)
 	},
 }
--- main.go
+++ main.go
@@ -2,6 +2,17 @@ package main
 
 import "github.com/dschanoeh/props/cmd"
 
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+	builtBy = "unknown"
+)
+
 func main() {
+	cmd.Version = version
+	cmd.Commit = commit
+	cmd.Date = date
+	cmd.BuiltBy = builtBy
 	cmd.Execute()
 }
